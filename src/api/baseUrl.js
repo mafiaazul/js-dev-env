0886be4887f51eeebce877515d322a60f2e345e4
mafiaazul <rodrigogrocha@hotmail.com>
2016-12-07 21:07:40 -0600

    test latest

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,8 +1,4 @@
 export default function getBaseUrl() {
-<<<<<<< HEAD
     const inDevelopment = window.location.hostname === 'localhost';
-=======
-    const inDevelopment = (global.window && global.window.location.hostname === 'localhost');
->>>>>>> 866a39652ccfbf5a355b8f298735942556a109a4
     return inDevelopment ? 'http://localhost:3001/' : '/';
 }
